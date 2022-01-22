b30e16585c28a8363856781d53424cba12b562f2
Alexey <88427011+alexspb1@users.noreply.github.com>
2022-01-22 14:06:41 +0300

    Update urls.py

--- conf/urls.py
+++ conf/urls.py
@@ -41,9 +41,10 @@ urlpatterns = [
     path('login', au_views.MyLoginView.as_view(), name='login_user'),
     path('register', au_views.MySignupView.as_view(), name='register'),
     path('logout', au_views.logout_user, name='logout_user'),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+] + static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
 
